2ecda120a05a04c3c387e4e5fba07a30e277c7f5
GSIII <kalseo901@gmail.com>
2025-06-10 06:24:27 +0000

    deploy.sh

--- sh/deploy.sh
+++ sh/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] $$ \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm use node
 
-cd acations
+cd actions
 
 git pull;
 
